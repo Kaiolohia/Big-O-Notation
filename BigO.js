6f62dee3f62eff85ad9cae0772144c5628395588
TruxSux <kaithomasdean@outlook.com>
2022-02-09 14:51:58 -1000

    Finished

---
+++ BigO.js
@@ -0,0 +1,46 @@
+/* 
+Part 1
+1. O(n)
+2. O(n)
+3. O(1)
+4. O(n^3)
+5. O(n)
+6. O(log(n))
+7. O(n log(n))
+8. O(2^n)
+9. O(1)
+10. O(n log(n)^10)
+*/
+
+/*
+Part 2
+
+logUpTo() has a time complexity of O(n)
+
+logAtLeast10() has a time complexity of O(10) [Loop]
+
+logAtMost10() has a time complexity of O(n)
+
+onlyElementsAtEvenIndex() has a time complexity of O(n)
+
+subtotals() has a time complexity of O(n^n)
+
+vowelCount() has a time complexity of O(n)
+*/
+
+/*
+Part 3
+
+1. True
+2. True
+3. False
+4. O(n)
+5. O(n)
+6. O(n)
+7. O(n log(n))
+8. O(n)
+9. O(1)
+10. O(n)
+11. O(1)
+12. O(n log(n))
+*/
